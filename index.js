65b38981564b0a4adc7c3d3aa9b526ba932b54d3
Marcel Niemiec <marcel.niemiec6@gmail.com>
2025-01-29 00:23:50 +0100

    feat: added correct implementation, works

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 require('dotenv').config()
 
 var app = express();
@@ -11,10 +13,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const { originalname, mimetype, size } = req.file;
+
+  if(!originalname || !mimetype || !size) {
+    return res.json({error: 'Error in file upload'});
+  }
+
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  });
 });
